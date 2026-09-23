96ddd8e8417ed462bae09134415b5b551d31ea0d
Anyi <daniela.09mh@gmail.com>
2026-09-22 21:21:06 -0500

    feat: Add unit tests and mutation tests

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov", "html"],
+      include: ["src/**/*.ts"],
+       exclude: [
+        "src/repositories/jsonRoomRepository.ts",
+        "src/cli.ts",
+       ]
+    },
+  },
+});
